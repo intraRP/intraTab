ae1d3956841f43e520770dad8f2293411c1bcf6b
hypax <github@hypax.wtf>
2026-03-13 09:23:06 +0100

    update escrow ignore

--- intraTab/fxmanifest.lua
+++ intraTab/fxmanifest.lua
@@ -41,7 +41,7 @@ data_file 'DLC_ITYP_REQUEST' 'stream/firetab.ytyp'
 escrow_ignore {
     'config.lua',
     'client/*.lua',
-    'server/billing_custom.lua',
+    'server/billing-custom.lua',
     'server/enotf_billing.lua',
     'server/main.lua',
     'html/**/*'
